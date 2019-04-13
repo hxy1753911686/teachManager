43d7f662f80608ab13b0c5be210d7cd4b892896f
hxy1753911686 <1753911686@qq.com>
2019-04-13 23:31:12 +0800

    ajax方式获取左边栏

---
+++ teachManager_springBoot/src/main/resources/static/js/treeMenu.js
@@ -0,0 +1,19 @@
+$(function () {
+
+    //ajax的方式获取左边栏
+    makeMenu = function (farLevel, farId) {
+        alert('farLevel:' + farLevel + 'farId:' + farId);
+
+        $.ajax({
+            type: "POST",
+            url: "/makeMenu",
+            data: {
+                id: farId,
+                level: farLevel
+            },
+            success: function (result) {
+                //拼接lefsider
+            }
+        })
+    }
+})
