39d9e537ce8b23d8a836d46bbe5e994ecf512875
hxy1753911686 <1753911686@qq.com>
2019-08-13 17:27:24 +0800

    着手准备修改user页面对角色的添加及更新

--- teachManager_springBoot/src/main/resources/sqlPre/estimate_role_permission.sql
+++ teachManager_springBoot/src/main/resources/sqlPre/estimate_role_permission.sql
@@ -1,4 +1,3 @@
-INSERT INTO estimate.role_permission (id, role_id, permission_id) VALUES (1, 1, 1);
-INSERT INTO estimate.role_permission (id, role_id, permission_id) VALUES (2, 2, 2);
-INSERT INTO estimate.role_permission (id, role_id, permission_id) VALUES (3, 2, 3);
-INSERT INTO estimate.role_permission (id, role_id, permission_id) VALUES (4, 2, 4);
+INSERT INTO estimate.role_permission (id, role_id, permission_id) VALUES (1, 1, '1');
+INSERT INTO estimate.role_permission (id, role_id, permission_id) VALUES (2, 2, '2,3,4,5,6');
+INSERT INTO estimate.role_permission (id, role_id, permission_id) VALUES (3, 14, '1,2,3,4,24');
